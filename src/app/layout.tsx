782465b446e6bcdc04d6b6e0e2cf1d10bbdaa4e8
Fikri Rakala <fikrirakala@gmail.com>
2024-10-28 16:13:47 +0800

    Setup fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../assets/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.className} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello</h1>
+      <h1 className="font-mono text-3xl font-bold">Hello</h1>
+    </>
+  );
 }
