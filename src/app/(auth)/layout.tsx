e3e719cd20634c92473665f67e6ed90877242a93
Fikri Rakala <fikrirakala@gmail.com>
2024-10-30 06:59:34 +0800

    Refactor auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-background bg-[url('/images/auth-light.png')] bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-[url('/images/auth-dark.png')]">
+    <main className="flex min-h-screen items-center justify-center bg-background px-4 py-10">
       {children}
     </main>
   );
