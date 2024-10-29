8f824d020ae1aabbc12eaa7aa1d0451c9934470c
Fikri Rakala <fikrirakala@gmail.com>
2024-10-29 10:27:06 +0800

    Install Shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello</h1>
       <h1 className="font-mono text-3xl font-bold">Hello</h1>
+      <Button>Click me</Button>
     </>
   );
 }
